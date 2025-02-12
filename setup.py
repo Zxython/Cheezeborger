431c95eacbf66547cb4fb7125f6c02cbf8c99ec5
Zxython <98050815+Zxython@users.noreply.github.com>
2025-02-12 15:23:06 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='cheezeborger',
+      version='1.0.0',
+      description='Disables all functions',
+      url='https://github.com/Zxython/Cheezeborger/',
+      author='Jerry Gillian',
+      author_email='jerrygillian822@gmail.com',
+      license='Free to use',
+      packages=['cheezeborger'],
+      zip_safe=False)
